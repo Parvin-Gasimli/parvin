9b762f5588e7cb28bd237737ca7f3ba747e9264f
parvin <parvinzga@code.edu.az>
2021-05-20 18:23:11 +0400

    javascript and html

---
+++ parvin.js
@@ -0,0 +1,76 @@
+let Name= "parvin";
+let lastName= "Gasimli";
+let  age  = 24;
+let yasadigiYer ="Baki seher Bineqedi Rayon Ayna sultanov 23A";
+let isPassed ="true";
+let x = 30
+let y = 23
+let z = x+y
+
+
+
+
+console.log(z);
+console.log(Name);
+console.log(lastName)
+console.log(isPassed)
+console.log(x)
+console.log(y)
+
+console.log(x+y);
+
+
+//arithmetic
+let a = 24;
+let b =34;
+let sum =a+b;
+let sub = a-b;
+let multi = a*b;
+let dev =a/b;
+let mood =a%b;
+let increment = ++a;
+let decerment = --b;
+
+console.log(sum)
+console.log(sub)
+console.log(multi) 
+console.log(dev)
+console.log(mood)
+console.log(increment)
+console.log(decerment)
+
+
+//comparsion&Relational
+ let greater =a>b;
+ let less =a<b;
+ let ge = a>=b;
+ let le = a<=b;
+ let e =a==b;
+ let ne = a!=b;
+
+
+
+
+ console.log(greater)
+ console.log(less)
+ console.log(ge)
+ console.log(le)
+ console.log(e)
+ console.log(ne)
+  
+
+ //asigment
+ let b=a
+ a=a+3,a+=3
+ a=a+5,a+=5
+ console.log(a);
+ console.log(b);
+
+
+
+
+
+
+
+
+
